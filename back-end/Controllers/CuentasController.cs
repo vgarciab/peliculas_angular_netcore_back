ff043dee97329d4ee144d39619fcb4fcd2034c24
vgarciab <vgb.sw.gestion@gmail.com>
2021-05-13 11:01:40 +0200

    127. Creando el CuentasController

---
+++ back-end/Controllers/CuentasController.cs
@@ -0,0 +1,100 @@
+﻿using back_end.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace back_end.Controllers
+{
+    [Route("api/cuentas")]
+    [ApiController]
+    public class CuentasController: ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly IConfiguration configuration;
+
+        public CuentasController(UserManager<IdentityUser> userManager,
+                                SignInManager<IdentityUser> signInManager,
+                                IConfiguration configuration)
+
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.configuration = configuration;
+        }
+
+
+        [HttpPost("crear")]
+        public async Task<ActionResult<RespuestaAutenticacion>> Crear([FromBody] CredencialesUsuario credenciales)
+        {
+            var usuario = new IdentityUser { UserName = credenciales.Email, Email = credenciales.Email };
+            var resultado = await userManager.CreateAsync(usuario, credenciales.Password);
+
+            if (resultado.Succeeded)
+            {
+                return await ConstruirToken(credenciales);
+            }
+            else
+            {
+                return BadRequest(resultado.Errors);
+            }
+        }
+
+
+        [HttpPost("login")]
+        public async Task<ActionResult<RespuestaAutenticacion>> Login([FromBody] CredencialesUsuario credenciales)
+        {
+            var resultado = await signInManager.PasswordSignInAsync(credenciales.Email, credenciales.Password,
+                isPersistent: false, lockoutOnFailure: false);
+
+            if (resultado.Succeeded)
+            {
+                return await ConstruirToken(credenciales);
+            }
+            else
+            {
+                return BadRequest("Login incorrecto");
+            }
+        }
+
+
+
+        private async Task<RespuestaAutenticacion> ConstruirToken(CredencialesUsuario credenciales)
+        {
+            // Claim es un conjunto de informaciones confiables acerca del usuario (Nombre, email...)
+            var claims = new List<Claim>()
+            {
+                new Claim("email", credenciales.Email) // Clave-Valor
+            };
+
+            var usuario = await userManager.FindByEmailAsync(credenciales.Email);
+            var claimsDB = await userManager.GetClaimsAsync(usuario); // con esto obtenemos el listado de Claims de la BDD del usuario.
+
+            claims.AddRange(claimsDB);
+
+            var llave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["llavejwt"]));
+            var creds = new SigningCredentials(llave, SecurityAlgorithms.HmacSha256);
+
+            var expiracion = DateTime.UtcNow.AddYears(1);  // El token va a ser válido por un año, en este caso.
+
+            var token = new JwtSecurityToken(issuer: null, audience: null, claims: claims,
+                                            expires: expiracion, signingCredentials: creds);
+
+            return new RespuestaAutenticacion()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiracion = expiracion
+            };
+        }
+
+    }
+
+}
